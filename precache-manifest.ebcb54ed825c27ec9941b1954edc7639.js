29bd387bc10d312b91675eda7c68488721a354c2
Zhahira <zhahira.s.r@mail.ugm.ac.id>
2020-04-15 15:11:16 +0700

    Updates

---
+++ precache-manifest.ebcb54ed825c27ec9941b1954edc7639.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4280ff099189099ee13232270c9f04",
+    "url": "/uts-web3/index.html"
+  },
+  {
+    "revision": "3028271c7e8c5cdc684c",
+    "url": "/uts-web3/static/css/2.2ff60e92.chunk.css"
+  },
+  {
+    "revision": "80d6b22e2797240f22d0",
+    "url": "/uts-web3/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3028271c7e8c5cdc684c",
+    "url": "/uts-web3/static/js/2.6eef2e6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uts-web3/static/js/2.6eef2e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80d6b22e2797240f22d0",
+    "url": "/uts-web3/static/js/main.1a917cbe.chunk.js"
+  },
+  {
+    "revision": "50a65fe1eef7899e2cbc",
+    "url": "/uts-web3/static/js/runtime-main.db85b1a4.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/uts-web3/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/uts-web3/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/uts-web3/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/uts-web3/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/uts-web3/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
